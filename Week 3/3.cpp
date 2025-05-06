679eb52bfb5e9241ebe02364120fc33145e14e4d
Yash Kirola <yashkirola12@gmail.com>
2025-05-06 23:34:21 +0530

    Update 3.cpp

--- Week 3/3.cpp
+++ Week 3/3.cpp
@@ -1,1 +1,40 @@
+#include <iostream>
+#include <algorithm> 
+using namespace std;
 
+bool hasDuplicates(int arr[], int n){
+    sort(arr, arr + n);
+
+    for (int i = 0; i < n - 1; i++){
+        if (arr[i] == arr[i + 1]){
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    int T;
+    cout << "Enter number of test cases: ";
+    cin >> T;
+
+    while (T--){
+        int n;
+        cout << "Enter array size: ";
+        cin >> n;
+
+        int arr[n];
+        cout << "Enter the elements: ";
+        for (int i = 0; i < n; i++){
+            cin >> arr[i];
+        }
+
+        if (hasDuplicates(arr, n)){
+            cout << "YES" << endl;
+        } else{
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
