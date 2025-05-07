2d11f6b9dc519b8a72beff44cc9f908505eafc90
Yash Kirola <yashkirola12@gmail.com>
2025-05-07 22:08:33 +0530

    Create 1.cpp

---
+++ Week 4/1.cpp
@@ -0,0 +1,60 @@
+void merge(int arr[], int l, int m, int r){
+    int n1 = m - l + 1;
+    int n2 = r - m;
+    int arr1[n1], arr2[n2];
+
+    for(int i = 0; i < n1; i++){
+        arr1[i] = arr[l + i];
+    }
+    for(int i = 0; i < n2; i++){
+        arr2[i] = arr[m + 1 + i];
+    }
+
+    int i = 0, j = 0, k = l;
+
+    while (i < n1 && j < n2) {
+        if (arr1[i] <= arr2[j]) {
+            arr[k++] = arr1[i++];
+        } else {
+            arr[k++] = arr2[j++];
+        }
+    }
+        
+    while (i < n1)
+        arr[k++] = arr1[i++];
+    while (j < n2)
+        arr[k++] = arr2[j++];
+
+}
+
+
+void mergeSort(int arr[], int l, int r){
+    if(l < r){
+        int m = l + (r-l)/2;
+        
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+
+        merge(arr, l, m, r);
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter the size of Array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the array: ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Enter the array: ";
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
