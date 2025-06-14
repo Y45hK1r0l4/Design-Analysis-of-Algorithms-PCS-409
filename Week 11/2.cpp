66e0a2e465e6825a5afce65f126c45d81ce5edef
Yash Kirola <yashkirola12@gmail.com>
2025-06-14 23:55:24 +0530

    Create 2.cpp

---
+++ Week 11/2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countWaysToMakeSum(int N, vector<int>& coins) {
+    vector<int> dp(N + 1, 0);
+    dp[0] = 1;  // 1 way to make sum 0 (use no coins)
+
+    for (int coin : coins) {
+        for (int i = coin; i <= N; ++i) {
+            dp[i] += dp[i - coin];
+        }
+    }
+
+    return dp[N];
+}
+
+int main() {
+    int coinCount;
+    cin >> coinCount;
+
+    vector<int> coins(coinCount);
+    for (int i = 0; i < coinCount; ++i) {
+        cin >> coins[i];
+    }
+
+    int targetSum;
+    cin >> targetSum;
+
+    int ways = countWaysToMakeSum(targetSum, coins);
+    cout << ways << endl;
+
+    return 0;
+}
