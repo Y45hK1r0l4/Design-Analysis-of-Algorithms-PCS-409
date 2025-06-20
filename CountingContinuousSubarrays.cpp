42ca94917d1b2534620ff99d641e587eb177d0fe
Yash Kirola <yashkirola12@gmail.com>
2025-06-20 23:52:47 +0530

    Update CountingContinuousSubarrays.cpp

--- CountingContinuousSubarrays.cpp
+++ CountingContinuousSubarrays.cpp
@@ -24,3 +24,85 @@ int main() {
     cout << "Total strictly increasing subarrays: " << countIncreasingSubarrays(arr) << endl;
     return 0;
 }
+
+
+
+
+
+int countSubarraysWithSumLessThanK(vector<int>& nums, int k) {
+    int count = 0;
+    int n = nums.size();
+
+    for (int i = 0; i < n; ++i) {
+        int sum = 0;
+        for (int j = i; j < n; ++j) {
+            sum += nums[j];
+            if (sum < k)
+                count++;
+            else
+                break; // no need to go further
+        }
+    }
+
+    return count;
+}
+
+
+
+
+
+int countSubarraysWithSumEqualToK(vector<int>& nums, int k) {
+    int count = 0;
+    int n = nums.size();
+
+    for (int i = 0; i < n; ++i) {
+        int sum = 0;
+        for (int j = i; j < n; ++j) {
+            sum += nums[j];
+            if (sum == k)
+                count++;
+        }
+    }
+
+    return count;
+}
+
+
+
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void countEvenOddSubarrays(vector<int>& nums) {
+    int evenCount = 0, oddCount = 0;
+    int n = nums.size();
+
+    for (int i = 0; i < n; ++i) {
+        int sum = 0;
+        for (int j = i; j < n; ++j) {
+            sum += nums[j];
+            if (sum % 2 == 0)
+                evenCount++;
+            else
+                oddCount++;
+        }
+    }
+
+    cout << "Subarrays with even sum: " << evenCount << endl;
+    cout << "Subarrays with odd sum: " << oddCount << endl;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3};
+    countEvenOddSubarrays(nums);
+    return 0;
+}
+
+
+
+
+
+
