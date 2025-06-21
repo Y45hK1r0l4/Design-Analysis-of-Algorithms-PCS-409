b12aba5f8f72595149fbecd53a00c12009c1432d
Yash Kirola <yashkirola12@gmail.com>
2025-06-21 22:27:56 +0530

    Update ChangeCoin.cpp

--- ChangeCoin.cpp
+++ ChangeCoin.cpp
@@ -22,3 +22,46 @@ int main() {
     cout << "Number of ways: " << countWays(coins, sum) << endl;
     return 0;
 }
+
+
+
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int countPermutations(int x, const vector<int>& coins) {
+    vector<int> dp(x + 1, 0);
+    dp[0] = 1;  // Base case: one way to make sum 0
+
+    for (int i = 1; i <= x; ++i) {
+        for (int coin : coins) {
+            if (i - coin >= 0) {
+                dp[i] += dp[i - coin];
+            }
+        }
+    }
+
+    return dp[x];
+}
+
+int main() {
+    int n, x;
+    cout << "Enter number of coins: ";
+    cin >> n;
+    
+    vector<int> coins(n);
+    cout << "Enter coin values:\n";
+    for (int i = 0; i < n; i++)
+        cin >> coins[i];
+
+    cout << "Enter target sum: ";
+    cin >> x;
+
+    int result = countPermutations(x, coins);
+    cout << "Total number of permutations to make " << x << " = " << result << endl;
+
+    return 0;
+}
